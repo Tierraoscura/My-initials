b7369075df653d6fa0cb12b88e2ddaa1b3a3accd
Carlos Garcia <85864280+Tierraoscura@users.noreply.github.com>
2021-06-17 01:31:56 -0700

    Add files via upload

---
+++ My initial's.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+//A fun fact about myself! I have none.
+
+int main() {
+
+  std::cout << "  C  C  C       G  G  G    \n";
+  std::cout << "C         C  G           G \n";
+  std::cout << "C            G             \n";
+  std::cout << "C            G  G  G  G  G \n";
+  std::cout << "C            G           G \n";
+  std::cout << "C         C  G           G \n";
+  std::cout << "  C  C  C       G  G  G    \n";
+
+}
